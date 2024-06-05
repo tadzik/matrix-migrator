12b4643a290c3337aa4ce75f48af6db232c28610
Tadeusz „tadzik” Sośnierz <tadeusz@sosnierz.com>
2024-06-05 12:36:12 +0200

    Display room issues on separate lines

--- src/web/RoomDetails.tsx
+++ src/web/RoomDetails.tsx
@@ -9,7 +9,11 @@ interface Props {
 export default function RoomDetails(props: Props) {
     return <div className='room-details'>
         { 
-            props.room.problems && props.room.problems.map((problem) => <MigratorErrorComponent key={ problem.technicalDetails } error={ problem } />)
+            props.room.problems && <div>
+                { props.room.problems.map((problem) => <>
+                        <MigratorErrorComponent key={ problem.technicalDetails } error={ problem } /> <br />
+                </>) }
+            </div>
         }
     </div>;
 }
